2b3d6a05995378dd25ef5a4346b4ebd60fd41376
gustijuno <gustijuno@gmail.com>
2021-11-23 14:20:41 +0700

    baru

---
+++ cypress/integration/UTS_spec.js
@@ -0,0 +1,58 @@
+
+context('Cypress Demo-Login', () =>{
+    it('UTS',() =>{
+        //Muhammad Gusti Herjuno (18)
+        //Login Test
+        //Scenario 1 (valid username & valid password)
+        //cy.visit('https://www.saucedemo.com/')
+        //cy.get('[data-test=username]').type('standard_user')
+        //cy.get('[data-test=password]').type('secret_sauce')
+        //cy.get('[data-test=login-button]').click()
+
+        //Scenario 2 (valid username & invalid password)
+        //cy.visit('https://www.saucedemo.com/')
+        //cy.get('[data-test=username]').type('standard_user')
+        //cy.get('[data-test=password]').type('salah')
+        //cy.get('[data-test=login-button]').click()
+
+        //Scenario 3 (invalid username & valid password)
+        //cy.visit('https://www.saucedemo.com/')
+        //cy.get('[data-test=username]').type('salah')
+        //cy.get('[data-test=password]').type('secret_sauce')
+        //cy.get('[data-test=login-button]').click()
+
+        //Scenario 4 (invalid username & invalid password)
+        //cy.visit('https://www.saucedemo.com/')
+        //cy.get('[data-test=username]').type('salah')
+        //cy.get('[data-test=password]').type('salah')
+        //cy.get('[data-test=login-button]').click()
+
+        //Scenario 5 (username = locked_out_user & valid pssword)
+        //cy.visit('https://www.saucedemo.com/')
+        //cy.get('[data-test=username]').type('locked_out_user')
+        //cy.get('[data-test=password]').type('salah')
+        //cy.get('[data-test=login-button]').click()
+
+
+        //Ella Amelia Widodo (11)
+        //Add card test
+     
+
+        //Nada Alya Kamila (19)
+        //Logout test
+      
+
+        //Zidan Shabira (26)
+        //Checkout test
+
+        
+        //Rizal Whisnu Wiratama (23)
+        //Sortir produk test  
+
+
+
+        //Risky Wisnu Oktovianto (22)
+        //Detail Product test
+
+    })
+})
---
+++ cypress/integration/login_spec.js
@@ -0,0 +1,9 @@
+
+context('Cypress Demo-Login', () =>{
+    it('login scenario',() =>{
+        cy.visit('https://www.saucedemo.com/')
+        cy.get('[data-test=username]').type('standard_user')
+        cy.get('[data-test=password]').type('secret_sauce')
+        cy.get('[data-test=login-button]').click()
+    })
+})
